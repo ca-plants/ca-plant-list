3955e4a9029b9aff154ea24d810a73306ed4e6b7
John Kenny <johnkenny54@yahoo.com>
2022-11-05 19:00:24 -0700

    Generalized tools.

--- build-site.js
+++ build-site.js
@@ -6,18 +6,13 @@ import { ErrorLog } from "./lib/errorlog.js";
 import { PageRenderer } from "./lib/pagerenderer.js";
 import commandLineArgs from "command-line-args";
 
-const OPTION_DEFS = [
-    { name: "datadir", type: String, defaultValue: "./data" },
-];
+const options = commandLineArgs( DataLoader.getOptionDefs() );
 
-const options = commandLineArgs( OPTION_DEFS );
-
-const TAXA_DATA_DIR = options.datadir;
 const CONFIG_DATA_DIR = "./data";
 const OUTPUT_DIR = "./output";
 
 Config.init( CONFIG_DATA_DIR );
-DataLoader.load( TAXA_DATA_DIR );
+DataLoader.load( options );
 PageRenderer.render( OUTPUT_DIR );
 
 ErrorLog.write( OUTPUT_DIR + "/errors.txt" );
