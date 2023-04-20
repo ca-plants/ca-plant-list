9edf995cf5193951ba86fa3d682aeffacf0e62f2
John Kenny <johnkenny54@yahoo.com>
2023-04-20 08:45:35 -0700

    Moved taxa and synonyms to ca-plant-list.

--- scripts/build-site.js
+++ scripts/build-site.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-import { DataLoader } from "./lib/dataloader.js";
-import { ErrorLog } from "./lib/errorlog.js";
-import { PageRenderer } from "./lib/pagerenderer.js";
+import { DataLoader } from "../lib/dataloader.js";
+import { ErrorLog } from "../lib/errorlog.js";
+import { PageRenderer } from "../lib/pagerenderer.js";
 import commandLineArgs from "command-line-args";
 
 const options = commandLineArgs( DataLoader.getOptionDefs() );
