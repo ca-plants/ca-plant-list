9dddb49267a0ae255b249751de8bd0032e939386
John Kenny <johnkenny54@yahoo.com>
2023-10-22 13:38:25 -0700

    Require instance for Config.

--- scripts/build-site.js
+++ scripts/build-site.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import { Config } from "../lib/config.js";
 import { DataLoader } from "../lib/dataloader.js";
 import { ErrorLog } from "../lib/errorlog.js";
 import { PageRenderer } from "../lib/pagerenderer.js";
@@ -9,6 +10,6 @@ const options = commandLineArgs( DataLoader.getOptionDefs() );
 
 const OUTPUT_DIR = "./output";
 
-PageRenderer.render( OUTPUT_DIR, DataLoader.load( options ) );
+PageRenderer.render( OUTPUT_DIR, new Config( options.datadir ), DataLoader.load( options ) );
 
 ErrorLog.write( OUTPUT_DIR + "/errors.tsv" );
