59caaa9b1d52c842f7cd25985ba460f124272426
John Kenny <johnkenny54@yahoo.com>
2025-02-09 09:39:22 -0800

    chore: fix utils.js TypeScript

--- jekyll/assets/js/ui.js
+++ jekyll/assets/js/ui.js
@@ -1,10 +1,10 @@
 class UI {
-
     static init() {
-        const tooltips = document.querySelectorAll( "span[title]" );
-        [ ...tooltips ].map( tooltipTriggerEl => new bootstrap.Tooltip( tooltipTriggerEl ) );
+        const tooltips = document.querySelectorAll("span[title]");
+        [...tooltips].map(
+            (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl),
+        );
     }
-
 }
 
 UI.init();
--- jekyll/assets/js/utils.js
+++ jekyll/assets/js/utils.js
@@ -1,26 +1,44 @@
-class Utils {
-
-    static domElement( name, attributes = {}, content ) {
-        const e = document.createElement( name );
-        for ( const [ k, v ] of Object.entries( attributes ) ) {
-            e.setAttribute( k, v );
+export class Utils {
+    /**
+     * @param {string} name
+     * @param {Object<string,string>} attributes
+     * @param {string} [content]
+     * @returns {Element}
+     */
+    static domElement(name, attributes = {}, content) {
+        const e = document.createElement(name);
+        for (const [k, v] of Object.entries(attributes)) {
+            e.setAttribute(k, v);
         }
-        if ( content ) {
+        if (content) {
             e.textContent = content;
         }
         return e;
     }
 
-    static domLink( href, text, attributes = {} ) {
-        const e = this.domElement( "a", Object.assign( { href: href }, attributes ) );
+    /**
+     * @param {string} href
+     * @param {string} text
+     * @param {Object<string,string>} attributes
+     * @returns {Element}
+     */
+    static domLink(href, text, attributes = {}) {
+        const e = this.domElement(
+            "a",
+            Object.assign({ href: href }, attributes),
+        );
         e.textContent = text;
         return e;
     }
 
-    static domTaxonLink( name ) {
-        return this.domLink( "./" + name.replaceAll( ".", "" ).replaceAll( " ", "-" ) + ".html", name );
+    /**
+     * @param {string} name
+     * @returns {Element}
+     */
+    static domTaxonLink(name) {
+        return this.domLink(
+            "./" + name.replaceAll(".", "").replaceAll(" ", "-") + ".html",
+            name,
+        );
     }
-
 }
-
-export { Utils };
