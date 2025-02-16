708ad6f0a7a733ec91dcf0fdae29448b86b34fe3
John Kenny <johnkenny54@yahoo.com>
2025-02-16 10:19:08 -0800

    chore: TypeScript updates

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -181,8 +181,6 @@ export class HTMLTaxon {
 }
 
 export class Jekyll {
-    static hasInclude(baseDir: string, path: string): boolean;
-    static include(fileName: string): string;
     static writeInclude(baseDir: string, path: string, data: string): void;
 }
 
